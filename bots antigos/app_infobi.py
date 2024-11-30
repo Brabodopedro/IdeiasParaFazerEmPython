02a5d8cacd7853bba068dc577f6868d2e2154e24
Pedro Henrique Brito Obara <106891475+Brabodopedro@users.noreply.github.com>
2024-11-30 14:48:54 -0400

    mudando a plataforma
    saindo da infobi e voltando pra ultramsg

--- bots antigos/app_infobi.py
+++ bots antigos/app_infobi.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import logging
-import json  # Importar o módulo json
-from ultrabot import ultraChatBot  # Certifique-se de que 'ultrabot.py' está no mesmo diretório
+from infobipbot import InfobipChatBot  # Certifique-se de que o arquivo `infobipbot.py` atualizado está no mesmo diretório
 
 app = Flask(__name__)
 
@@ -11,6 +10,7 @@ logging.basicConfig(level=logging.INFO)
 @app.route('/', methods=['POST'])
 def webhook():
     try:
+        # Obtendo os dados do webhook
         json_data = request.get_json()
         logging.info(f"Dados recebidos: {json_data}")
 
@@ -25,28 +25,18 @@ def webhook():
 
         message_data = json_data['data']
 
-        # Verificar o tipo de message_data
-        logging.info(f"Tipo de message_data: {type(message_data)}")
-        logging.info(f"Conteúdo de message_data: {message_data}")
-
-        # Se message_data for uma string, parsear como JSON
-        if isinstance(message_data, str):
-            logging.info("message_data é uma string, parseando como JSON")
-            message_data = json.loads(message_data)
-
-        # Verifique se 'body' e 'from' estão presentes em 'message_data'
-        if 'body' not in message_data or 'from' not in message_data:
-            logging.error("Faltando 'body' ou 'from' nos dados recebidos.")
-            return jsonify({'error': 'Faltando body ou from nos dados'}), 400
-
-        # Instancie o bot com 'message_data'
-        bot = ultraChatBot(message_data)
+        # Instancie o bot com os dados recebidos
+        bot = InfobipChatBot(message_data)
         response = bot.Processing_incoming_messages()
-        return jsonify({'status': 'sucesso', 'response': response}), 200
+        logging.info(f"Resposta do bot: {response}")
+
+        # Retorna uma resposta ao webhook
+        return jsonify({'status': 'sucesso'}), 200
 
     except Exception as e:
         logging.error(f"Ocorreu um erro no webhook: {e}")
         return jsonify({'error': 'Erro interno do servidor'}), 500
 
 if __name__ == '__main__':
+    # Inicia o servidor Flask
     app.run(debug=True)
