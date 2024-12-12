35c6c6b8d2da3fd42a3ee319977e3f2978c1c5e1
Pedro Henrique Brito Obara <106891475+Brabodopedro@users.noreply.github.com>
2024-12-12 12:22:56 -0400

    a

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 from flask import Flask, request, jsonify
 import logging
 import json
-from ultrabot import ultraChatBot  # Certifique-se de que 'ultrabot.py' está no mesmo diretório
+from ultrabot import ultraChatBot  # Certifique-se que o nome do arquivo continua ultrabot.py ou ajuste aqui
 from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
 import time
 import pandas as pd
 
-
 app = Flask(__name__)
 
-# Configuração do logging para exibir mensagens no console
 logging.basicConfig(level=logging.INFO)
 
-# Importar as funções globais do ultrabot
 from ultrabot import load_states, save_states, send_message
 
-# Definir a função que verifica conversas inativas
 def check_inactive_conversations():
     try:
         states = load_states()
@@ -24,21 +20,17 @@ def check_inactive_conversations():
         for chatID, state_info in list(states.items()):
             last_interaction = state_info.get('last_interaction', current_time)
             if current_time - last_interaction > 10 * 60 and state_info['state'] != 'SESSION_ENDED':
-                # Envia mensagem ao usuário
                 send_message(chatID, "Sua sessão foi encerrada por inatividade. Se precisar de algo, por favor, envie uma nova mensagem para iniciar um novo atendimento.")
-                # Atualiza o estado para 'SESSION_ENDED'
                 states[chatID]['state'] = 'SESSION_ENDED'
                 states[chatID]['pause_start_time'] = time.time()
         save_states(states)
     except Exception as e:
         logging.error(f"Erro ao verificar conversas inativas: {e}")
 
-# Inicializar o scheduler
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=check_inactive_conversations, trigger="interval", seconds=60)
 scheduler.start()
 
-# Encerra o scheduler ao sair da aplicação
 atexit.register(lambda: scheduler.shutdown())
 
 @app.route('/', methods=['POST'])
@@ -51,28 +43,23 @@ def webhook():
             logging.error("Nenhum dado JSON recebido.")
             return jsonify({'error': 'Nenhum dado recebido'}), 400
 
-        # Verifique se 'data' está presente no JSON
         if 'data' not in json_data:
             logging.error("Faltando 'data' no JSON recebido.")
             return jsonify({'error': 'Faltando dados no JSON'}), 400
 
         message_data = json_data['data']
 
-        # Verificar o tipo de message_data
         logging.info(f"Tipo de message_data: {type(message_data)}")
         logging.info(f"Conteúdo de message_data: {message_data}")
 
-        # Se message_data for uma string, parsear como JSON
         if isinstance(message_data, str):
             logging.info("message_data é uma string, parseando como JSON")
             message_data = json.loads(message_data)
 
-        # Verifique se 'body' e 'from' estão presentes em 'message_data'
         if 'body' not in message_data or 'from' not in message_data:
             logging.error("Faltando 'body' ou 'from' nos dados recebidos.")
             return jsonify({'error': 'Faltando body ou from nos dados'}), 400
 
-        # Instancie o bot com 'message_data'
         bot = ultraChatBot(message_data)
         response = bot.Processing_incoming_messages()
         return jsonify({'status': 'sucesso', 'response': response}), 200
